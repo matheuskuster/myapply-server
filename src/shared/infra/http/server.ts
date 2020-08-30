c7c0876d58b7592b17d905e382f1af3b2c7e2de9
Matheus Kuster <matheuskuster@hotmail.com>
2020-08-29 23:05:01 -0300

    Add shared folder

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,17 +1,52 @@
+import 'express-async-errors';
 import 'reflect-metadata';
-import express from 'express';
+import 'dotenv/config';
 
+import express, {Request, Response, NextFunction} from 'express';
+import {errors} from 'celebrate';
+
+import AppError from '@shared/errors/AppError';
 import routes from '@shared/infra/http/routes';
 
 import '@shared/infra/typeorm';
+import '@shared/container';
 
-const app = express();
+function handleError(
+    err: Error,
+    _request: Request,
+    response: Response,
+    _next: NextFunction,
+) {
+    if (err instanceof AppError) {
+        return response.status(err.statusCode).json({
+            message: err.message,
+            status: 'error',
+        });
+    }
 
-app.use(express.json());
-app.use(routes);
+    console.error(err);
 
-app.listen(3333, () => {
-    console.log('🚀 Server listening on port 3333');
-});
+    return response.status(500).json({
+        message: 'Internal server error.',
+        status: 'error',
+    });
+}
 
-export default app;
+function server() {
+    const app = express();
+
+    app.use(express.json());
+    app.use(routes);
+
+    app.use(errors());
+
+    app.use(handleError);
+
+    const PORT = process.env.SERVER_PORT || 3333;
+
+    app.listen(PORT, () => {
+        console.log(`🚀 Server listening on port ${PORT}`);
+    });
+}
+
+server();
