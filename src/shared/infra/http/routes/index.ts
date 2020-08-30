ab0459c5256036901263abcb68a5148340837ed9
Matheus Kuster <matheuskuster@hotmail.com>
2020-08-30 12:25:43 -0300

    Expose sessions routes to express

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,9 +1,11 @@
 import {Router} from 'express';
 
+import sessionsRouter from '@modules/user/infra/http/routes/sessions.routes';
 import userRouter from '@modules/user/infra/http/routes/users.routes';
 
 const routes = Router();
 
 routes.use('/users', userRouter);
+routes.use('/sessions', sessionsRouter);
 
 export default routes;
